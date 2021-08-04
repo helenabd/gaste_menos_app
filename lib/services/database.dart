336d2388b76f6263eff3b9c4a63685f4a2ea44ea
Helena Brito Dias <helenabdias95@gmail.com>
2021-08-04 12:37:34 -0300

    firestore service

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,46 +1,26 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
 import 'package:gaste_menos_app/domain/domain.dart';
 import 'package:gaste_menos_app/services/api_path.dart';
+import 'package:gaste_menos_app/services/services.dart';
 
 abstract class Database {
   Future<void> createDesp(Desp desp);
   Stream<List<Desp>> despStream();
-  Stream<List<T>> _collectionsStrem<T>(
-      {@required String path,
-      @required T Function(Map<String, dynamic> data) builder});
 }
 
 class FirestoreDatabase implements Database {
   final String uid;
+  final _service = FirestoreService.instance;
   FirestoreDatabase({
     @required this.uid,
   }) : assert(uid != null);
 
-  Future<void> createDesp(Desp desp) => _setData(
+  Future<void> createDesp(Desp desp) => _service.setData(
         path: APIPath.desp(uid, 'desp_abc'),
         data: desp.toMap(),
       );
 
-  Stream<List<Desp>> despStream() => _collectionsStrem(
+  Stream<List<Desp>> despStream() => _service.collectionsStrem(
       path: APIPath.despesas(uid), builder: (data) => Desp.fromMap(data));
-
-  Future<void> _setData({String path, Map<String, dynamic> data}) async {
-    final reference = FirebaseFirestore.instance.doc(path);
-    print('$path: $data');
-    await reference.set(data);
-  }
-
-  Stream<List<T>> _collectionsStrem<T>(
-      {@required String path,
-      @required T Function(Map<String, dynamic> data) builder}) {
-    final reference = FirebaseFirestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) => snapshot.docs
-        .map((snapshot) => builder(
-              snapshot.data(),
-            ))
-        .toList());
-  }
 }
--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,3 +1,4 @@
 export 'auth.dart';
 export 'email_and_password_validators.dart';
 export 'database.dart';
+export 'firestore_service.dart';
