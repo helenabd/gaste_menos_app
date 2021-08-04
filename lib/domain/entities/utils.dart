fadc3fe8d986fc9af243cc555741519a9c11ea2a
Helena Brito Dias <helenabdias95@gmail.com>
2021-08-04 19:31:24 -0300

    format data

--- lib/domain/entities/utils.dart
+++ lib/domain/entities/utils.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Utils {
   static double totalHeight({BuildContext context}) {
@@ -7,5 +8,28 @@ class Utils {
 
   static double totalWidth({BuildContext context}) {
     return MediaQuery.of(context).size.width;
+  }
+
+  static String hours(double hours) {
+    final hoursNotNegative = hours < 0.0 ? 0.0 : hours;
+    final formatter = NumberFormat.decimalPattern();
+    final formatted = formatter.format(hoursNotNegative);
+    return '${formatted}h';
+  }
+
+  static String date(DateTime date) {
+    return DateFormat.yMMMd().format(date);
+  }
+
+  static String dayOfWeek(DateTime date) {
+    return DateFormat.E().format(date);
+  }
+
+  static String currency(double pay) {
+    if (pay != 0.0) {
+      final formatter = NumberFormat.simpleCurrency(decimalDigits: 0);
+      return formatter.format(pay);
+    }
+    return '';
   }
 }
